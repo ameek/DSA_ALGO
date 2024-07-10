82eed49c2ae0cff99205edd1302ca28b0d293eb5
Ameek <ameek.aziz@gmail.com>
2024-07-10 19:41:12 +0600

    creating a unidirectional NODE for linked list

--- DSA/linkedList.py
+++ DSA/linkedList.py
@@ -6,3 +6,40 @@
 # A linked list is a collection of nodes where each node contains a value and a reference to the next node in the list.
 # The first node is called the head and the last node is called the tail.
 # The tail points to None, which indicates the end of the list.
+
+# Node class
+#this node is unidirectional
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+    def getValue(self):
+        return self.data
+
+    def setValue(self, data):
+        self.data = data
+    
+    def getNext(self):
+        return self.next
+    
+    def setNext(self, next):
+        self.next = next
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+
+print(a)
+print(b)
+print(c)
+
+print(a.getValue())
+a.setNext(b)
+print(a.getNext().getValue())
+b.setNext(c)
+print(a.getNext().getNext().getValue())
+
+# how to access pointer in python?
+# using the __dict__ attribute  
