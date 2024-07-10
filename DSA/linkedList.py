f6923cceec4e19c6e2f46c554f7764c8d200b202
Ameek <ameek.aziz@gmail.com>
2024-07-10 19:02:00 +0600

    feat: Add implementation of linked list in Python init

---
+++ DSA/linkedList.py
@@ -0,0 +1,8 @@
+# creating a linked list from scratch using python
+# what is a linked list?
+# A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations.
+# The elements in a linked list are linked using pointers
+# A linked list is represented by a pointer to the first node of the linked list.
+# A linked list is a collection of nodes where each node contains a value and a reference to the next node in the list.
+# The first node is called the head and the last node is called the tail.
+# The tail points to None, which indicates the end of the list.
